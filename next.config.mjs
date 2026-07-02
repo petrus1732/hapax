2f2e020f8a6e86b4b7bd5a6d35b985e7538107bb
HwH-Deadliner <b10902022@g.ntu.edu.tw>
2026-07-03 03:46:46 +0800

    Fix workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,14 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'hapax';
-
 const nextConfig = {
   env: {
     POSTGRES_URL: process.env.POSTGRES_URL,
   },
-
-  output: 'export',
-  trailingSlash: true,
-
-  images: {
-    unoptimized: true,
+  outputFileTracing: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/dictionary': ['./app/lib/dictionary.json'],
+    },
   },
-
-  basePath: isGithubActions ? `/${repoName}` : '',
-  assetPrefix: isGithubActions ? `/${repoName}/` : '',
 };
 
 export default nextConfig;
